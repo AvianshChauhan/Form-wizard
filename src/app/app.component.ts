2bdfd71080e209a358a01bc817320a5a7c86eaa3
avinash_chauhan <avinash.chauhan@agrikheti.com>
2023-07-22 14:40:29 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray, AbstractControl, Form } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Test';
+
+  userForm: FormGroup;
+  addressForm: FormGroup;
+  paymentForm: FormGroup;
+  currentModel: any;
+  pivotIndex: any;
+  arr: any = []
+  constructor(
+    private fb: FormBuilder
+  ) {
+    
+
+    this.userForm = this.fb.group({
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required]
+    })
+
+    this.addressForm = this.fb.group({
+      city:['',Validators.required],
+      pincode:['',[Validators.required, Validators.pattern('^[0-9]+$')]]
+    })
+
+    this.paymentForm = this.fb.group({
+      bnk_number: ['',Validators.required],
+      ifsc_code: ['',Validators.required]
+    })
+  }
+
+  ngOnInit() {
+    this.currentModel = 'userForm';
+  }
+  
+  onNext(data) {
+    this.currentModel = data
+  }
+
+
+  back(data) {
+    this.currentModel = data
+  }
+
+
+  // onSubmit() {
+  //   // You can now access all the form data and display the summary or take any other action here.
+  //   console.log('Form Data:', {
+  //     personalInfo: this.userForm.value,
+  //     address: this.addressForm.value,
+  //     paymentDetail: this.paymentForm.value,
+  //   });
+  // }
+
+
+  getProgressValue(): number {
+    const totalSteps = 3; // Total number of form steps (Personal Information, Address, Payment Detail)
+    let completedSteps = 0;
+
+    // Check the currentModel to determine the number of completed steps
+    if (this.currentModel === 'addressForm') {
+      completedSteps = 1;
+    } else if (this.currentModel === 'paymentForm') {
+      completedSteps = 2;
+    } else if (this.currentModel === 'summary') {
+      completedSteps = 3;
+    }
+
+    return (completedSteps / totalSteps) * 100;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
